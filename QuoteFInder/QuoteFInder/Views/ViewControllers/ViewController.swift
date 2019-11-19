0dc8a211a60379c25296aba77c5849031e8fee2c
RYAN GREENBURG <ryangreenburg@RYANs-MacBook-Pro-2.local>
2019-11-19 11:35:09 -0700

    Initial Commit

---
+++ QuoteFInder/QuoteFInder/Views/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuoteFInder
+//
+//  Created by RYAN GREENBURG on 11/19/19.
+//  Copyright © 2019 RYAN GREENBURG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
